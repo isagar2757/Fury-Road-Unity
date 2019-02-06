6dfed1fed83a5d8a9985fe496e8c31c3b5942295
Sagar <isagar2757@gmail.com>
2019-02-06 15:32:12 -0500

    Assets Added
    Colliders and Rigid Body Added

---
+++ COMP305-W2019/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //PUBLIC VARIABLES
+    public float speed = 10.0f;
+    public float jumpForce = 500.0f;
+
+
+    //PRIVATE VARIABLES
+    private Rigidbody2D rBody;
+
+    //Reserved function runs only once when the object is created 
+    //Used for initialization
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+
+        void Update()
+    {
+       
+    }
+    void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+
+        rBody.velocity = new Vector2(horiz*speed, rBody.velocity.y);
+       
+    }
+}
